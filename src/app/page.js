6f4b0e0f12b054cc9f093107773eb7d9b1a51e2f
Brandon Sun <brandonsun75@gmail.com>
2024-12-14 16:12:59 -0800

    Remove comment

--- src/app/page.js
+++ src/app/page.js
@@ -42,10 +42,10 @@ export default function Home() {
     billingCity: "",
     billingProvince: "",
     billingZip: "",
-    billingCountry: ""
-  });  
+    billingCountry: "",
+  });
 
-  const [warningMessage, setWarningMessage] = useState('');
+  const [warningMessage, setWarningMessage] = useState("");
 
   // Handle form data changes
   const handleInputChange = (e) => {
@@ -73,31 +73,42 @@ export default function Home() {
       orderDetails.billingCountry
     );
   };
-  
+
   const calculateCharge = (quantity) => Math.round(6.99 * quantity * 100);
 
   const handleSubmitOrder = async () => {
-
     if (areFieldsFilled()) {
       // Proceed with order submission
-      console.log('Form submitted');
-      setWarningMessage('Please fill out all fields');
-      console.log("Total amount to charge:", calculateCharge(orderDetails.quantity));
-      //await sendOrderEmail(orderDetails); //< -- turn this shit off when testing
+      console.log("Form submitted");
+      setWarningMessage("Please fill out all fields");
+      console.log(
+        "Total amount to charge:",
+        calculateCharge(orderDetails.quantity)
+      );
+      await sendOrderEmail(orderDetails);
       setStep(2);
     } else {
       // Show warning message if fields are not filled
-      setWarningMessage('Please fill out all fields!');
+      setWarningMessage("Please fill out all fields!");
     }
   };
 
   const renderOrderDetails = () => (
-    <div style={{ backgroundColor: '#f0f0f0', minHeight: '100vh', height: 'auto' }}>
-      <img src="/images/SecondSavour_Banner.png" alt="Logo" style={{ minWidth: '100%', maxWidth: '100%', height: 'auto' }} />
+    <div
+      style={{ backgroundColor: "#f0f0f0", minHeight: "100vh", height: "auto" }}
+    >
+      <img
+        src="/images/SecondSavour_Banner.png"
+        alt="Logo"
+        style={{ minWidth: "100%", maxWidth: "100%", height: "auto" }}
+      />
       <h2 className="order-header">Order Details</h2>
-      <h3 className="order-subheader">*Please note that online purchases are only available for the Lower Mainland region</h3>
+      <h3 className="order-subheader">
+        *Please note that online purchases are only available for the Lower
+        Mainland region
+      </h3>
       <form className="order-form">
-      <div className="form-group">
+        <div className="form-group">
           <label>Select Quantity of Citrus Treats Packages:</label>
           <input
             type="number"
@@ -112,7 +123,6 @@ export default function Home() {
           />
         </div>
 
-       
         <div className="form-group">
           <h3>Contact Details</h3>
           <label>Email:</label>
@@ -169,43 +179,45 @@ export default function Home() {
             }
           />
         </div>
-        
-          <div className="form-group">
-            <label>City:</label>
-            <input
-              type="text"
-              value={orderDetails.city}
-              onChange={(e) =>
-                setOrderDetails({ ...orderDetails, city: e.target.value })
-              }
-              style={{ width: '100%' }}
-            />
-          </div>
-          <div className="form-group">
-            <label>Province:</label>
-            <input
-              type="text"
-              value={orderDetails.province}
-              onChange={(e) =>
-                setOrderDetails({ ...orderDetails, province: e.target.value })
-              }
-              style={{ width: '100%' }}
-            />
-          </div>
-          <div className="form-group">
-            <label>Postal/ZIP Code:</label>
-            <input
-              type="text"
-              value={orderDetails.zip}
-              onChange={(e) =>
-                setOrderDetails({ ...orderDetails, zip: e.target.value })
-              }
-              style={{ width: '100%' }}
-            />
-          </div>
 
-          <div className="form-group">
-          <h3>Billing Details (card payment will be requested in the next page) </h3>
+        <div className="form-group">
+          <label>City:</label>
+          <input
+            type="text"
+            value={orderDetails.city}
+            onChange={(e) =>
+              setOrderDetails({ ...orderDetails, city: e.target.value })
+            }
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Province:</label>
+          <input
+            type="text"
+            value={orderDetails.province}
+            onChange={(e) =>
+              setOrderDetails({ ...orderDetails, province: e.target.value })
+            }
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Postal/ZIP Code:</label>
+          <input
+            type="text"
+            value={orderDetails.zip}
+            onChange={(e) =>
+              setOrderDetails({ ...orderDetails, zip: e.target.value })
+            }
+            style={{ width: "100%" }}
+          />
+        </div>
+
+        <div className="form-group">
+          <h3>
+            Billing Details (card payment will be requested in the next page){" "}
+          </h3>
           <label>Billing Name:</label>
           <input
             type="text"
@@ -216,13 +228,16 @@ export default function Home() {
           />
         </div>
 
-          <div className="form-group">
+        <div className="form-group">
           <label>Billing Country:</label>
           <input
             type="text"
             value={orderDetails.billingCountry}
             onChange={(e) =>
-              setOrderDetails({ ...orderDetails, billingCountry: e.target.value })
+              setOrderDetails({
+                ...orderDetails,
+                billingCountry: e.target.value,
+              })
             }
           />
         </div>
@@ -233,47 +248,53 @@ export default function Home() {
             type="text"
             value={orderDetails.billingAddress}
             onChange={(e) =>
-              setOrderDetails({ ...orderDetails, billingAddress: e.target.value })
+              setOrderDetails({
+                ...orderDetails,
+                billingAddress: e.target.value,
+              })
             }
           />
         </div>
-        
-          <div className="form-group">
-            <label>Billing City:</label>
-            <input
-              type="text"
-              value={orderDetails.billingCity}
-              onChange={(e) =>
-                setOrderDetails({ ...orderDetails, billingCity: e.target.value })
-              }
-              style={{ width: '100%' }}
-            />
-          </div>
-          <div className="form-group">
-            <label>Billing Province:</label>
-            <input
-              type="text"
-              value={orderDetails.billingProvince}
-              onChange={(e) =>
-                setOrderDetails({ ...orderDetails, billingProvince: e.target.value })
-              }
-              style={{ width: '100%' }}
-            />
-          </div>
-          <div className="form-group">
-            <label>Billing Postal/ZIP Code:</label>
-            <input
-              type="text"
-              value={orderDetails.billingZip}
-              onChange={(e) =>
-                setOrderDetails({ ...orderDetails, billingZip: e.target.value })
-              }
-              style={{ width: '100%' }}
-            />
-          </div>          
-        
+
+        <div className="form-group">
+          <label>Billing City:</label>
+          <input
+            type="text"
+            value={orderDetails.billingCity}
+            onChange={(e) =>
+              setOrderDetails({ ...orderDetails, billingCity: e.target.value })
+            }
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Billing Province:</label>
+          <input
+            type="text"
+            value={orderDetails.billingProvince}
+            onChange={(e) =>
+              setOrderDetails({
+                ...orderDetails,
+                billingProvince: e.target.value,
+              })
+            }
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Billing Postal/ZIP Code:</label>
+          <input
+            type="text"
+            value={orderDetails.billingZip}
+            onChange={(e) =>
+              setOrderDetails({ ...orderDetails, billingZip: e.target.value })
+            }
+            style={{ width: "100%" }}
+          />
+        </div>
+
         {/* Warning message */}
-        {warningMessage && <p style={{ color: 'red' }}>{warningMessage}</p>}
+        {warningMessage && <p style={{ color: "red" }}>{warningMessage}</p>}
 
         {/* Submit Button */}
 
@@ -299,7 +320,7 @@ export default function Home() {
           Next
         </button>
       </form>
-  
+
       <style>
         {`
           .order-header {
@@ -348,47 +369,77 @@ export default function Home() {
   const renderPaymentDetails = () => {
     const totalPrice = (orderDetails.quantity * 6.99).toFixed(2); // Calculate and format total price
 
-    const rawAmount = parseFloat(totalPrice)
+    const rawAmount = parseFloat(totalPrice);
     const taxAmount = (totalPrice * 0.05).toFixed(2); // Calculate 5% tax
     const shippingAmount = 2; // Shipping cost
-    const finalPrice = (rawAmount + parseFloat(taxAmount) + shippingAmount).toFixed(2); // Add tax to the original amount
+    const finalPrice = (
+      rawAmount +
+      parseFloat(taxAmount) +
+      shippingAmount
+    ).toFixed(2); // Add tax to the original amount
 
     return (
-      <div style={{ backgroundColor: '#f0f0f0', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: '100%', marginBottom: '30px' }}>
-          <img src="/images/treats.png" alt="Logo" style={{ width: '40%', height: 'auto', marginRight: '20px' }} />
+      <div
+        style={{
+          backgroundColor: "#f0f0f0",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            maxWidth: "100%",
+            marginBottom: "30px",
+          }}
+        >
+          <img
+            src="/images/treats.png"
+            alt="Logo"
+            style={{ width: "40%", height: "auto", marginRight: "20px" }}
+          />
           <div>
             <h2 className="payment-header">Payment Details</h2>
             <p className="payment-text">Subtotal: ${rawAmount} CAD</p>
             <p className="payment-text">Tax: ${taxAmount} CAD</p>
             <p className="payment-text">Shipping: ${shippingAmount} CAD</p>
-            <p className="payment-text">Total Cost: ${finalPrice} CAD <br /> <br /> </p>
-            <p className="payment-text">Upon successful payment, you will be redirected</p>
+            <p className="payment-text">
+              Total Cost: ${finalPrice} CAD <br /> <br />{" "}
+            </p>
+            <p className="payment-text">
+              Upon successful payment, you will be redirected
+            </p>
             <p className="payment-text">Otherwise, no charges will be made</p>
           </div>
         </div>
-    
+
         <PaymentForm
           //production
           applicationId="sq0idp-leAPDrw-4oMD6vQXYKhZWA"
           locationId="LVYPE74W6JCDC"
-
           //sandbox
           //applicationId="sandbox-sq0idb-ayXDRhsJiwimurKk9RNYcA"
           //locationId="LXV6Q4QQXQFCT"
 
           cardTokenizeResponseReceived={async (token) => {
-            const result = await submitPayment(token.token, orderDetails.quantity);
+            const result = await submitPayment(
+              token.token,
+              orderDetails.quantity
+            );
             console.log(result);
             setStep(3);
           }}
         >
-          <div style={{margin: "0 auto" }}>
+          <div style={{ margin: "0 auto" }}>
             <CreditCard />
           </div>
         </PaymentForm>
-    
+
         <style>
           {`
             .payment-header {
@@ -410,35 +461,66 @@ export default function Home() {
         </style>
       </div>
     );
-    
-    
   };
-  
+
   const renderThankYou = () => {
     return (
-      <div style={{ backgroundColor: '#f0f0f0', minHeight: '100vh', textAlign: 'center', padding: '20px' }}>
+      <div
+        style={{
+          backgroundColor: "#f0f0f0",
+          minHeight: "100vh",
+          textAlign: "center",
+          padding: "20px",
+        }}
+      >
         {/* Banner Image */}
-        <img 
-          src="/images/SecondSavour_Banner.png" 
-          alt="Logo" 
-          style={{ minWidth: '100%', maxWidth: '100%', height: 'auto', marginBottom: '20px' }} 
+        <img
+          src="/images/SecondSavour_Banner.png"
+          alt="Logo"
+          style={{
+            minWidth: "100%",
+            maxWidth: "100%",
+            height: "auto",
+            marginBottom: "20px",
+          }}
         />
-  
+
         {/* Thank You Message */}
-        <h2 style={{ color: '#333', fontSize: '28px', margin: '20px 0' }}>Thank you for your order!</h2>
-        <p style={{ color: '#555', fontSize: '18px', lineHeight: '1.6', maxWidth: '600px', margin: '0 auto' }}>
-          We’ve received your order and are processing it. You’ll receive a confirmation email shortly with the details. 
-          If you have any questions, feel free to <a href="mailto:secondsavour@enactussfu.com" style={{ color: '#007BFF' }}>contact us</a>.
+        <h2 style={{ color: "#333", fontSize: "28px", margin: "20px 0" }}>
+          Thank you for your order!
+        </h2>
+        <p
+          style={{
+            color: "#555",
+            fontSize: "18px",
+            lineHeight: "1.6",
+            maxWidth: "600px",
+            margin: "0 auto",
+          }}
+        >
+          We’ve received your order and are processing it. You’ll receive a
+          confirmation email shortly with the details. If you have any
+          questions, feel free to{" "}
+          <a
+            href="mailto:secondsavour@enactussfu.com"
+            style={{ color: "#007BFF" }}
+          >
+            contact us
+          </a>
+          .
         </p>
-  
+
         {/* Optional Additional Content */}
-        <p style={{ marginTop: '30px', fontSize: '16px', color: '#777' }}>
-          Explore more on our <a href="https://www.secondsavour.ca/" style={{ color: '#007BFF' }}>homepage</a>.
+        <p style={{ marginTop: "30px", fontSize: "16px", color: "#777" }}>
+          Explore more on our{" "}
+          <a href="https://www.secondsavour.ca/" style={{ color: "#007BFF" }}>
+            homepage
+          </a>
+          .
         </p>
       </div>
     );
   };
-  
 
   return (
     <div>
